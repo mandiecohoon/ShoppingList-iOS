aaf57b80b6c74c5ab523d4432737f577adad794e
Amanda Cohoon <iimandie@gmail.com>
2014-03-22 16:32:14 -0400

    Initial Commit

---
+++ ShoppingList/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  ShoppingList
+//
+//  Created by Amanda Cohoon on 2014-03-22.
+//  Copyright (c) 2014 Amanda Cohoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShoppingList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShoppingList
+//
+//  Created by Amanda Cohoon on 2014-03-22.
+//  Copyright (c) 2014 Amanda Cohoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLAppDelegate class]));
+    }
+}
